ab3942c823df32486cf80a2927adcc4267809520
cmlx <cmlx1218@163.com>
2019-06-20 21:00:19 +0800

    feat:测试一下

---
+++ src/main/java/com/fosterpet/cmlx/fosterpetcmlxtest/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.fosterpet.cmlx.fosterpetcmlxtest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author cmlx
+ * @Date 2019-6-20 0020 20:19
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/getUserInfo")
+    public String getUserInfo(String userName){
+        return "我是"+userName;
+    }
+
+}
---
+++ src/test/java/com/fosterpet/cmlx/fosterpetcmlxtest/FosterpetCmlxTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fosterpet.cmlx.fosterpetcmlxtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FosterpetCmlxTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
